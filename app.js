6bd4ec5afd959873dc272389b3c313d15e24a48b
Colin Peters <colinthedev@gmail.com>
2021-08-24 14:41:02 -0600

    finished displaying data from api, tweaked margins lined up columns

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // Weather data
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const descElementDaily = document.querySelector(".daily-description p");
-const locationElement = document.querySelector(".location p");
-const windElement = document.querySelector(".wind-value");
-const humidityElement = document.querySelector(".humidity-value");
+const locationElement = document.querySelector(".location p");              // Location
+const iconElement = document.querySelector(".weather-icon");                // Icon
+const tempElement = document.querySelector(".temperature-value p");         // Temp
+const descElement = document.querySelector(".temperature-description p");   // Short description
+const descElementDaily = document.querySelector(".daily-description p");    // Daily description
+const sunRiseElement = document.querySelector(".sun-rise");                 // Sunrise
+const sunSetElement = document.querySelector(".sun-set");                   // Sunset
+const windElement = document.querySelector(".wind-value");                  // Wind speed
+const gustElement = document.querySelector(".wind-gusts");                  // Gust speed
+const feelsLikeElement = document.querySelector(".feelslike-value");        // Feels-like
+const humidityElement = document.querySelector(".humidity-value");          // Humidity 
+const tempMinElement = document.querySelector(".temp_min");                 // Temp_min
+const tempMaxElement = document.querySelector(".temp_max");                 // Temp_max
 // Notification
-const notificationElement = document.querySelector(".notification");
+const notificationElement = document.querySelector(".notification");        // Notification
 
 // Store data 
 const weather = {}
@@ -56,14 +62,52 @@ function getWeather(latitude, longitude) {
             weather.country = data.sys.country;                              // Country name
             weather.iconId = data.weather[0].icon;                           // Icon
             weather.temperature.value = Math.floor(data.main.temp - KELVIN); // Temp
-            weather.description = data.weather[0].description;               // Description
+            weather.description = data.weather[0].description;               // Short description
+            weather.sunrise = convertTime(data.sys.sunrise);                 // Sunrise
+            weather.sunset = convertTime(data.sys.sunset);                   // Sunset
             weather.wind = Math.round(data.wind.speed);                      // Wind speed
+            weather.gust = Math.round(data.wind.gust);                       // Gust speed
+            weather.feels_like = Math.floor(data.main.feels_like - KELVIN);  // Feels-like
             weather.humidity = data.main.humidity;                           // Humidity 
-        })
+            weather.temp_min = Math.floor(data.main.temp_min - KELVIN);      // Temp_min
+            weather.temp_max = Math.floor(data.main.temp_max - KELVIN);      // Temp_max
+        }) 
         .then(function () {
             displayBackground();
             displayWeather();
         });
+}
+
+// Display weather data to UI
+function displayWeather() {
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;      // City name Country name
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;     // Icon
+    tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;  // Temp
+    descElement.innerHTML = `${weather.description}`;                       // Short description
+    descElementDaily.innerHTML = dailyDescription();                        // Daily description
+    sunRiseElement.innerHTML = `Sunrise <br>${weather.sunrise} AM`;         // Sunrise
+    sunSetElement.innerHTML = `Sunset <br>${weather.sunset} PM`;            // Sunset
+    windElement.innerHTML = `Wind <br>${weather.wind} mph`;                 // Wind speed
+    gustElement.innerHTML = `Gust <br>${weather.gust} mph`;                 // Gust speed
+    feelsLikeElement.innerHTML = `Feels-like <br>${weather.feels_like} °F`; // Feels-like
+    humidityElement.innerHTML = `Humidity <br>${weather.humidity}%`;        // Humidity 
+    tempMinElement.innerHTML = `Min-temp <br>${weather.temp_min} °F`;       // Temp_min
+    tempMaxElement.innerHTML = `Max-temp <br>${weather.temp_max} °F`;       // Temp_max
+}
+
+// Daily description of weather forecast
+function dailyDescription() {
+    return `No meatballs today: ${weather.description} currently. The high will be ${weather.temp_max} °F with a low of ${weather.temp_min} °F`;
+}
+
+// Convert sunrise sunset times 
+function convertTime(unixTime) {
+    let dt = new Date(unixTime * 1000)
+    let h = dt.getHours() - 12
+    let m = "0" + dt.getMinutes()
+    let t = h + ":" + m.substr(-2)
+    return t.includes('-') ? t.substring(1)
+        : t
 }
 
 // Change background depending on if its day/night (using icon to check)
@@ -79,16 +123,6 @@ function displayBackground() {
     } else if (nightTime == true) {
         background.style.backgroundImage = `${nightBackground}`;
     }
-}
-
-// Display weather data to UI
-function displayWeather() {
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;      // City name Country name
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;     // Icon
-    tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;  // Temp
-    descElement.innerHTML = `${weather.description}`;                       // Description
-    windElement.innerHTML = `wind <br>${weather.wind} mph`;                 // Wind speed
-    humidityElement.innerHTML = `humidity <br>${weather.humidity}%`;        // Humidity 
 }
 
 // Convert to C
